e93a4f5bcfd4f17ccc5d5124b932347ffe0b8bd0
eb22 <eb22.plugin.developer@gmail.com>
2015-11-27 17:00:11 -0600

    Create Playermessages.java
    :Playermessage: created
    :description: player join message

---
+++ Commands/Playermessages.java
@@ -0,0 +1,14 @@
+public class PlayerJoin implements Listener {
+
+  public Commands plugin;
+  
+  @EventHandler
+  public void onPlayerJoin(PlayerJoin event) {
+  Player player = event.getPlayer();
+  if(player.hasPlayedBefore()) {
+    Bukkit.broadcastMessage(ChatColor.YELLOW + ":COC: Welcome back to the Clash of Clans server " + player.getname() " "!");
+    } else {
+    Bukkit.broadcastMessage(ChatColor.RED + ":COC: Welcome to the server " + player.getName() + "your new so have a good time!");
+    }
+  }
+}  
